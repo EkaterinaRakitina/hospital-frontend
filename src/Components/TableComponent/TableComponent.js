0dc8c1d654fb206541c1a36d4edd956354838d0b
EkaterinaRakitina <erakitina.exceedteam@gmail.com>
2022-02-02 17:57:55 +0300

    add table appointment

--- src/Components/TableComponent/TableComponent.js
+++ src/Components/TableComponent/TableComponent.js
@@ -1,11 +1,43 @@
 import React from 'react';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+} from '@mui/material';
 import './TableComponent.scss';
 
 const TableComponent = () => {
   return (
     <div className="Table-container">
       <div className="Table-wrapper">
-
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell align="center">Имя</TableCell>
+                <TableCell align="center">Врач</TableCell>
+                <TableCell align="center">Дата</TableCell>
+                <TableCell align="center">Жалобы</TableCell>
+                <TableCell align="center"></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+                <TableRow
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row"></TableCell>
+                  <TableCell align="center"></TableCell>
+                  <TableCell align="center"></TableCell>
+                  <TableCell align="center"></TableCell>
+                  <TableCell align="center"></TableCell>
+                </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
       </div>
     </div>
   );
